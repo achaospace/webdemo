e59b35fdd6c3093a71821b089623203903aa1a30
achao <kxxmsy@gmail.com>
2019-09-07 17:11:46 +0800

    change githubhref
    change githubhref

--- byui/dist/lay/modules/mytool.js
+++ byui/dist/lay/modules/mytool.js
@@ -1,2 +1,2 @@
-/** byui-v2.5.4 By Achao github - www.baidu.com */
+/** byui-v2.5.4 By Achao github - https://github.com/achaospace/webdemo */
  ;byui.define(["jquery"],function(o){"use strict";byui.$;o("mytool",function(){console.log("test")})});
